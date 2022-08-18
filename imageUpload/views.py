8763612dd3bf85fb46875c3f14c903dfa4860050
RohilPrajapati <rohilprajapati@gmail.com>
2022-08-18 12:03:57 +0545

    added custom name to the uploaded file and check the file size before uploading max file size should be less the 300kb

--- imageUpload/views.py
+++ imageUpload/views.py
@@ -1,19 +1,32 @@
 from rest_framework.views import APIView
 from .models import Image
-from django.http import request
 from .serializers import ImageSerializer
 from rest_framework.response import Response
-from rest_framework.parsers import FormParser,MultiPartParser,JSONParser
 from rest_framework import status
+import os
+from datetime import datetime
 
 
 class ImageUploadView(APIView):
-    # parser_classes = [MultiPartParser]
+    def get(self,request,format=None):
+        image = Image.objects.all();
+        serializer = ImageSerializer(image,many=True)
+        return Response(serializer.data )
     def post(self,request,format=None):
         serializer = ImageSerializer(data= request.data)
         # print(serializer.file)
         if serializer.is_valid(raise_exception=True):
-            print(serializer.data)
+            if(serializer.validated_data['image'].size > 307200):
+                response = {
+                    'message':'image size cant me more then 300KB'
+                }
+                return Response(response,status=status.HTTP_406_NOT_ACCEPTABLE)
+            name = serializer.validated_data['image'].name
+            file_name, ext = os.path.splitext(name)
+            now = datetime.now()
+            date_time = now.strftime("%Y%m$d%H%M%S")
+            custom_file_name = "CN"+date_time+ext
+            serializer.validated_data['image'].name = custom_file_name
             serializer.save()
             image = request.data.get('image')
             return Response({
